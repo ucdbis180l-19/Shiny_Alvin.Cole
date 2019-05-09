b2e440343634c46e8eebd9d181ef105960e6d0be
Cole <colwilliams@ucdavis.edu>
2019-05-09 22:42:55 +0000

    Changed y lab

--- Shiny_App/server.R
+++ Shiny_App/server.R
@@ -13,14 +13,19 @@ library(tidyverse)
 rice_data <- read.table("Rice_Pheno.txt",header=TRUE)
 rice_data$Population <- as.character(rice_data$assignedPop)
 
+y_labels <- c("Aluminum Tolerance","Plant height", "Panicle length","Seed length","Seed width")
+name_data <- matrix(data=y_labels,nrow=1,ncol=5)
+colnames(name_data) <- c("Alu.Tol","Plant.height","Panicle.length","Seed.length","Seed.width")
+
 shinyServer(function(input, output) {
-   
+  
+  
   output$violinPlot <- renderPlot({
     
     pl <- ggplot(data=rice_data,
                  aes_string(x=input$group,
                             y=input$trait))
-    pl + geom_violin()
+    pl + geom_violin(fill="light blue") + ylab(name_data[,input$trait])
     
   })
   
